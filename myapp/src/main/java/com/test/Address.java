b392c6f8598bb47f4adbbe01b17f799569082476
dhseo <donghyeon9403@naver.com>
2024-08-05 14:47:28 +0900

    ok

---
+++ myapp/src/main/java/com/test/Address.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Address {
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    
+}
--- myapp/src/main/java/com/test/Main.java
+++ myapp/src/main/java/com/test/Main.java
@@ -1,7 +1,13 @@
 package com.test;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Employee employee = context.getBean(Employee.class);
+        System.out.println(employee.getAddress().getLocation());
+        context.close();
     }
 }
